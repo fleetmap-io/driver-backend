a12338d0f055c3ae4776fe626ba2f2f97023cead
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-01-28 13:49:18 +0000

    added eslint added body-parser

--- src/app.js
+++ src/app.js
@@ -1,39 +1,41 @@
-const CognitoExpress = require("cognito-express")
+const CognitoExpress = require('cognito-express')
 const express = require('express')
-const devices = require("./devices");
-const cors = require("cors");
+const devices = require('./devices')
+const bodyParser = require('body-parser')
 
 const cognitoExpress = new CognitoExpress({
-    region: "us-east-1",
-    cognitoUserPoolId: process.env.COGNITO_USER_POOOL_ID,
-    tokenUse: "access", //Possible Values: access | id
-    tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
+  region: 'us-east-1',
+  cognitoUserPoolId: process.env.COGNITO_USER_POOOL_ID,
+  tokenUse: 'access', // Possible Values: access | id
+  tokenExpiration: 3600000 // Up to default expiration of 1 hour (3600000 ms)
 })
 
+// eslint-disable-next-line new-cap
 const app = new express()
 // noinspection JSCheckFunctionSignatures
-app.use(cors())
+app.use(require('cors'))
+app.use(bodyParser.json())
 
-app.use(async function(req, res, next) {
-    const accessTokenFromClient = req.headers.authorization
+app.use(async function (req, res, next) {
+  const accessTokenFromClient = req.headers.authorization
 
-    if (!accessTokenFromClient) return res.status(401).send("Access Token missing from header")
+  if (!accessTokenFromClient) return res.status(401).send('Access Token missing from header')
 
-    await cognitoExpress.validate(accessTokenFromClient.replace('Bearer ',''), function (err, response) {
-        if (err) return res.status(401).send(err);
-        res.locals.user = response;
-        next();
-    })
+  await cognitoExpress.validate(accessTokenFromClient.replace('Bearer ', ''), function (err, response) {
+    if (err) return res.status(401).send(err)
+    res.locals.user = response
+    next()
+  })
 })
 
 app.get('*', async (req, resp) => {
-    console.log(resp.locals.user)
-    resp.json(await devices.get(resp.locals.user))
+  console.log(resp.locals.user)
+  resp.json(await devices.get(resp.locals.user))
 })
 
 app.post('*', async (req, res) => {
-    console.log(req.path, req.body)
-    res.json({statusCode: 200})
+  console.log(req.path, req.body)
+  res.json({ statusCode: 200 })
 })
 
 module.exports = app
