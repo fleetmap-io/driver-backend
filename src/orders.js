7795a02ac31e0520440d7948e2510eda33f65b93
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-01-29 18:23:10 +0000

    almost there

--- src/orders.js
+++ src/orders.js
@@ -4,16 +4,17 @@ const axios = require('axios')
 
 exports.addPhoto = async ({ photoID, photoURL, name }, user) => {
   const database = users.getOdooDB(user)
-  const order = await odoo.executeKw(database, 'fsm.order', 'search_read', [['name', '=', name]])
-  console.log('order [0]', order[0])
-  const image = await axios.get(photoURL, { responseType: 'arraybuffer' }).then(r => Buffer.from(r.data, 'base64'))
+  const [order] = await odoo.executeKw(database, 'fsm.order', 'search_read', [['name', '=', name]])
+  console.log('order', order)
+  const image = await axios.get(photoURL, { responseType: 'arraybuffer' })
+    .then(r => Buffer.from(r.data, 'binary').toString('base64'))
   await odoo.executeKw(database,
     'ir.attachment',
     'create',
     [{
       res_model: 'fsm.order',
       name: 'image',
-      res_id: order[0].id,
+      res_id: order.id,
       type: 'binary',
       mimetype: 'image/png',
       store_fname: 'image.png',
