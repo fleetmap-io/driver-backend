7ee5e5f2c92479bfce52560d2d134f409a806620
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-01-29 16:17:51 +0000

    debug search order

--- src/orders.js
+++ src/orders.js
@@ -3,6 +3,6 @@ const users = require('./users')
 
 exports.addPhoto = async ({ photoID, photoURL, name }, user) => {
   const database = users.getOdooDB(user)
-  const order = await odoo.executeKw(database, 'fsm.order', 'search_count', [])
+  const order = await odoo.executeKw(database, 'fsm.order', 'search_red', [['Name', '=', name]])
   console.log(order)
 }
