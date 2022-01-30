9af45e3a567ce539e38946e40636b7d8e85c2961
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-01-30 15:46:27 +0000

    starging get order

--- src/app.js
+++ src/app.js
@@ -19,9 +19,7 @@ app.use(bodyParser.json())
 
 app.use(async function (req, res, next) {
   const accessTokenFromClient = req.headers.authorization
-
   if (!accessTokenFromClient) return res.status(401).send('Access Token missing from header')
-
   await cognitoExpress.validate(accessTokenFromClient.replace('Bearer ', ''), function (err, response) {
     if (err) return res.status(401).send(err)
     res.locals.user = response
@@ -29,13 +27,15 @@ app.use(async function (req, res, next) {
   })
 })
 
+app.get('/orders/:orderId', async (req, resp) => {
+  resp.json(await orders.getOrder(req.params.orderId, resp.locals.user))
+})
+
 app.get('*', async (req, resp) => {
-  console.log(resp.locals.user)
   resp.json(await devices.get(resp.locals.user))
 })
 
 app.post('/addPhoto', async (req, res) => {
-  console.log(req.path, req.body, res.locals.user)
   res.json(await orders.addPhoto(req.body, res.locals.user))
 })
 
--- src/orders.js
+++ src/orders.js
@@ -2,6 +2,11 @@ const odoo = require('./odoo')
 const users = require('./users')
 const axios = require('axios')
 
+exports.getOrder = async (orderId, user) => {
+  const database = users.getOdooDB(user)
+  return [await odoo.executeKw(database, 'fsm.order', 'search_read', [['id', '=', orderId]])]
+}
+
 exports.addPhoto = async ({ photoID, photoURL, name }, user) => {
   const database = users.getOdooDB(user)
   const [order] = await odoo.executeKw(database, 'fsm.order', 'search_read', [['name', '=', name]])
