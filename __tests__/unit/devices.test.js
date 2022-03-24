18ba4f3400e27b057ff6fc13f4d77856cea3ba78
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-03-24 21:48:04 +0000

    marshall in scan

--- __tests__/unit/devices.test.js
+++ __tests__/unit/devices.test.js
@@ -1,11 +1,11 @@
 // Import all functions from hello-from-lambda.js
-const lambda = require('../../src/devices');
+const lambda = require('../../src/devices')
 
 // This includes all tests for helloFromLambdaHandler()
 describe('Test devices', function () {
-    // This test invokes helloFromLambdaHandler() and compare the result
-    it('Verifies successful response', async () => {
-        const result = await lambda.get({username:'teste8'});
-        expect(result).toHaveLength(15);
-    })
+  // This test invokes helloFromLambdaHandler() and compare the result
+  it('Verifies successful response', async () => {
+    const result = await lambda.get({ username: 'teste8' })
+    expect(result).toHaveLength(15)
+  })
 })
