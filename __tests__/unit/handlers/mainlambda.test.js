e8fe1183d03c287e4df9b699ec5b58a3b75143c9
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-03-24 22:33:21 +0000

    almost there

--- __tests__/unit/handlers/mainlambda.test.js
+++ __tests__/unit/handlers/mainlambda.test.js
@@ -1,20 +1,11 @@
 // Import all functions from hello-from-lambda.js
-const lambda = require('../../../src/handlers');
+const devices = require('../../../src/devices')
 
 // This includes all tests for helloFromLambdaHandler()
-describe('Test lambda', function () {
-    // This test invokes helloFromLambdaHandler() and compare the result
-    it('Verifies successful response', async () => {
-        // Invoke helloFromLambdaHandler()
-        const result = await lambda.handler();
-        /*
-            The expected result should match the return from your Lambda function.
-            e.g.
-            if you change from `const message = 'Hello from Lambda!';` to `const message = 'Hello World!';` in hello-from-lambda.js
-            you should change the following line to `const expectedResult = 'Hello World!';`
-        */
-        const expectedResult = 'Hello from Lambda!';
-        // Compare the result with the expected result
-        expect(result).toEqual(expectedResult);
-    });
-});
+// eslint-disable-next-line no-undef
+describe('Test lambda', async function () {
+  it('test', async () => {
+    process.env.DEVICES_TABLE = 'DevicesIgnitionOff'
+    console.log(await devices.get('qgz36fh9veng0b8dsokrgv'))
+  })
+})
