a59ecb82c05c4cf89d9d0395fcaeccb757ab6cc8
joaquimcardeira <joaquim_cardeira@hotmail.com>
2022-03-24 23:31:05 +0000

    simplify

--- src/devices.js
+++ src/devices.js
@@ -1,7 +1,6 @@
 const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager')
 const client = new SecretsManagerClient({ region: 'us-east-1' })
 const _secret = client.send(new GetSecretValueCommand({ SecretId: process.env.TRACCAR_SECRET_NAME })).then(s => JSON.parse(s.SecretString))
-const axios = require('axios')
 const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb')
 const dynamo = new DynamoDBClient({ region: 'us-east-1' })
 const { unmarshall, marshall } = require('@aws-sdk/util-dynamodb')
@@ -18,14 +17,14 @@ exports.get = async (token, user) => {
   const d = unmarshall(ddevice.Items[0])
   console.log('device', d)
   const auth = await _secret
-  const [device] = await axios.get(`${auth.basePath}/devices?id=${d.deviceId}`, { auth }).then(d => d.data)
+  const axios = require('axios').create({ auth, baseURL: auth.basePath })
+  const [device] = await axios.get(`devices?id=${d.deviceId}`).then(d => d.data)
   console.log(device)
   device.attributes.driverUniqueId = user.username
-  await axios.put(`${auth.basePath}/devices/${device.id}`, device, { auth })
-  const permission = { deviceId: device.id, attributeId: 37 }
-  const computed = await axios.get(`${auth.basePath}/attributes/computed/${device.id}`, { auth }).then(d => d.data)
+  await axios.put(`devices/${device.id}`, device)
+  const computed = await axios.get(`attributes/computed/${device.id}`).then(d => d.data)
   if (!computed.find(a => a.id === 37)) {
-    await axios.post(`${auth.basePath}/permissions`, permission, { auth })
+    await axios.post('permissions', { deviceId: device.id, attributeId: 37 })
   }
   return device
 }
