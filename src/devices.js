40c1f97f1f0727d4891d603a7fe48867a076c10b
jcardus <asklocation.net@gmail.com>
2022-07-11 14:47:11 +0100

    log

--- src/devices.js
+++ src/devices.js
@@ -1,29 +1,41 @@
 const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager')
 const client = new SecretsManagerClient({ region: 'us-east-1' })
 const _secret = client.send(new GetSecretValueCommand({ SecretId: process.env.TRACCAR_SECRET_NAME })).then(s => JSON.parse(s.SecretString))
-const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb')
+const { DynamoDBClient, ScanCommand, GetItemCommand } = require('@aws-sdk/client-dynamodb')
 const dynamo = new DynamoDBClient({ region: 'us-east-1' })
 const { unmarshall, marshall } = require('@aws-sdk/util-dynamodb')
+const axios = require('axios')
 
 exports.get = async (token, user) => {
   console.log('get', token, user)
-  const dDevice = await dynamo.send(new ScanCommand({
-    TableName: process.env.DEVICES_TABLE,
-    FilterExpression: '#token = :token',
-    ExpressionAttributeValues: marshall({ ':token': token }, { removeUndefinedValues: true }),
-    ExpressionAttributeNames: { '#token': 'token' }
-  }))
-  console.log(dDevice)
-  const d = unmarshall(dDevice.Items[0])
-  console.log('device', d)
-  const auth = await _secret
-  const axios = require('axios').create({ auth, baseURL: auth.baseUrl })
-  const [device] = await axios.get(`devices?id=${d.deviceId}`).then(d => d.data)
-  device.attributes.driverUniqueId = user.username
-  await axios.put(`devices/${device.id}`, device)
-  const computed = await axios.get('attributes/computed?deviceId=' + device.id).then(d => d.data)
-  if (!computed.find(a => a.id === 37)) {
-    await axios.post('permissions', { deviceId: device.id, attributeId: 37 })
+  if (token) {
+    const dDevice = await dynamo.send(new ScanCommand({
+      TableName: process.env.DEVICES_TABLE,
+      FilterExpression: '#token = :token',
+      ExpressionAttributeValues: marshall({ ':token': token }, { removeUndefinedValues: true }),
+      ExpressionAttributeNames: { '#token': 'token' }
+    }))
+    console.log(dDevice)
+    const d = unmarshall(dDevice.Items[0])
+    console.log('device', d)
+    const auth = await _secret
+    const axios = require('axios').create({ auth, baseURL: auth.baseUrl })
+    const [device] = await axios.get(`devices?id=${d.deviceId}`).then(d => d.data)
+    device.attributes.driverUniqueId = user.username
+    await axios.put(`devices/${device.id}`, device)
+    const computed = await axios.get('attributes/computed?deviceId=' + device.id).then(d => d.data)
+    if (!computed.find(a => a.id === 37)) {
+      await axios.post('permissions', { deviceId: device.id, attributeId: 37 })
+    }
+    return device
+  } else {
+    console.log('getting', user.username)
+    const _user = await dynamo.send(new GetItemCommand({
+      TableName: process.env.DRIVER_USER_TABLE,
+      Key: marshall({ id: user.username })
+    }))
+    user = unmarshall(_user.Item)
+    const secret = await _secret
+    return await axios.get(`${secret.basePath}/devices?userId=${user.parentUserId}`, { auth: secret }).then(d => d.data)
   }
-  return device
 }
